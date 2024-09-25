f2c2495ead8897c12f106253ea75644578e4f7b1
Arun <notoriousarun@protonmail.com>
2024-09-25 11:39:41 +0530

    Remove extra import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './Compoents/MainPage';
 import BookList from './Compoents/BookList';
